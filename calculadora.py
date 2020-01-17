a564f4bddd1704e9afc3470c665afedd74add5b0
andresro30 <andresrochag15@hotmail.com>
2020-01-16 21:27:43 -0500

    Mejora de pruebas

--- calculadora.py
+++ calculadora.py
@@ -6,19 +6,26 @@ a = [3,-5]
 b = [-6,-4]
 ans = [0,0]
 
+def rad_grados(num):
+    return (180.0/math.pi)*num
+
 def suma(a,b):
+    ans=[0,0]
     ans[0] = a[0]+b[0]
     ans[1] = a[1]+b[1]
+    return ans
     
 def resta(a,b):
+    ans=[0,0]
     ans[0] = a[0]-b[0]
     ans[1] = a[1]-b[1]
-
+    return ans
 
 
 def multiplicacion(a,b):
     ent = []
     ima = []
+    ans=[0,0]
     for i in range(len(a)):
         for j in range(len(b)):    
             if(i==0 and j==0):
@@ -30,41 +37,52 @@ def multiplicacion(a,b):
             else:
                 ent.append(-1*(a[i]*b[j]))
 
-    print(ent,ima)
+    #print(ent,ima)
     ans[0]=ent[0]+ent[1]
     ans[1]=ima[0]+ima[1]
+    return ans
 
 def division(a,b):
-    z1 = a[1]**2 + b[1]**2
-    z2 = a[0]*a[1] + b[0]*b[1]
-    z3 = a[1]*b[0] - a[0]*b[1]
+    ans=[0,0]
+    z1 = a[0]*b[0] + a[1]*b[1]
+    z2 = a[1]*b[0] - a[0]*b[1]
+    z3 = b[0]**2 + b[1]**2
 
-    ans[0] = z2/z1
-    ans[1] = z3/z1
-    print(ans)
-
+    ans[0] = z1/z3
+    ans[1] = z2/z3
+    return ans
 
 def modulo(a):
+    ans=[0,0]
     temp = a[0]**2 + a[1]**2
     ans[0] = temp**(1/2)
+    return ans
 
 def conjugado(a):
+    ans=[0,0]
     ans[0] = a[0]
     ans[1] = a[1]*-1    
+    return ans
 
 def polarCartesiano(a):
     #Los angulos estan en radianes
+    ans=[0,0]
     r = a[0]
     ang = a[1]
     ans[0] = r*(math.cos(ang))
     ans[1] = r*(math.sin(ang))
+    return ans
 
 def cartesianoPolar(a):
+    ans=[0,0]
     ans[0] = (a[0]**2 + a[1]**2)**(1/2)
     ans[1] = math.atan(a[1]/a[0])
+    return ans
 
 def fase(a):
+    ans=[0,0]
     ans[0] = math.atan2(a[1],a[0])
+    return ans
     
 def prettyPrinting(c):
     if(c[1]==0):
@@ -76,6 +94,7 @@ def prettyPrinting(c):
         else:
             print("",str(c[1])+"i")
 
+
     else:
         if(c[1]==1):
             print("i")
@@ -83,12 +102,7 @@ def prettyPrinting(c):
             print(str(c[1])+"i")
 
 def main():
-    #suma(a,b)
-    #multiplicacion(a,b)
-    #division(a,b)
-    #modulo(a)
-    #conjugado(a)
-    #polarCartesiano(2,120)
-    fase([1.2,-1.5])
-    prettyPrinting(ans)
+    op = polarCartesiano([13,23])
+    prettyPrinting(op)
+
 main()
--- test.py
+++ test.py
@@ -1,15 +1,54 @@
 import unittest
-import Complex
+import calculadora
 
 
-class Test(unittest.TestCase):
+class TestCalculadora(unittest.TestCase):
 
-    def testSuma(self):
+    def test_suma(self):
         cor1 = [4,6]
         cor2 = [1,-5]
-        ans = Complex.suma(cor1,cor2)
-        self.assertEquals(ans,[5,1])
+        ans = calculadora.suma(cor1,cor2)
+        self.assertEqual(ans,[5,1])
 
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_resta(self):
+        cor1 = [4,18]
+        cor2 = [1,-5]
+        ans = calculadora.resta(cor1,cor2)
+        self.assertEqual(ans,[3,23])
+
+    def test_multiplicacion(self):
+        cor1 = [4,6]
+        cor2 = [1,4]
+        ans = calculadora.multiplicacion(cor1,cor2)
+        self.assertEqual(ans,[-20,22])
+
+    def test_division(self):
+        cor1 = [3,2]
+        cor2 = [1,-2]
+        ans = calculadora.division(cor1,cor2)
+        self.assertEqual(ans,[-0.2,1.6])
+
+    def test_modulo(self):
+        cor1 = [1,5]
+        cor2 = [3,7]
+        self.assertEqual(calculadora.modulo(cor1)[0],5.0990195135927845)
+        self.assertEqual(calculadora.modulo(cor2)[0],7.615773105863909)
+
+    def test_conjugado(self):
+        cor1 = [3,21]
+        cor2 = [6,-76]
+        self.assertEqual(calculadora.conjugado(cor1),[3,-21])
+        self.assertEqual(calculadora.conjugado(cor2),[6,76])
+
+    #def test_polarCartesiano(self):
+
+    #def test_cartesianoPolar(self):
+
+    #def test_fase(self):
+
+    
+        
+
+if __name__ == "__main__":
+    unittest.main()
